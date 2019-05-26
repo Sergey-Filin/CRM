067c40d6839ce29bf3e5f7f76ba49e6d462356a5
Sergey-Filin <sergeyfilin9999@gmail.com>
2019-05-26 14:47:39 +0300

    OK

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,11 @@
-if (procces.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys.prod");
 } else {
   module.exports = require("./keys.dev");
 }
+
+// module.exports = {
+//   mongoURI: "mongodb://localhost:27017/mydatabasedb",
+// 	// mongoURI: "mongodb+srv://sergeyfilin9999:<06712574271gf!>@cluster0-al5vd.mongodb.net/test?retryWrites=true",
+// 	jwt: "dev-jwt"
+// };
